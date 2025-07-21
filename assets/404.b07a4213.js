07577928290dcb224d587e3bb5d0cb52b269db36
omarakosha <omerakosha@gmail.com>
2025-07-21 12:15:19 +0300

    Updates

--- assets/404.b07a4213.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./index.2eb496d9.js";import{V as o,j as t,ad as i,ae as n}from"./vendor.05ff05e8.js";const u=()=>{const e=o(),{formatMessage:a}=s();return t(n,{status:"404",title:"404",subTitle:a({id:"gloabal.tips.notfound"}),extra:t(i,{type:"primary",onClick:()=>e("/"),children:a({id:"gloabal.tips.backHome"})})})};export{u as default};
